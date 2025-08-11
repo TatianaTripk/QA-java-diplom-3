be8807856f4cd648df14dfa6b80f47515533606c
Tatiana Tripkovich <tripkovich.t@yandex.ru>
2025-08-11 18:43:27 +0300

    new commit

---
+++ src/main/java/praktikum/pages/ResetPasswordPage.java
@@ -0,0 +1,16 @@
+package praktikum.pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ResetPasswordPage {
+    private WebDriver driver;
+    public ResetPasswordPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    private final By resetPasswordForm = By.xpath("//div[@class='Auth_login__3hAey']");
+    public void checkIfResetPasswordFormIsDisplayed() {
+        Assert.assertTrue(driver.findElement(resetPasswordForm).isDisplayed());
+    }
+}
