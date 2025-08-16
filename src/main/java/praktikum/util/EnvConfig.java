604bdc6528469e832f55b3d2a8c9e2170be02b68
Tatiana Tripkovich <tripkovich.t@yandex.ru>
2025-08-17 01:46:27 +0300

    new commit

--- src/main/java/praktikum/util/EnvConfig.java
+++
@@ -1,7 +0,0 @@
-package praktikum.util;
-
-public class EnvConfig {
-
-    public static final String VALID_EMAIL = "logintest@gmail.com";
-    public static final String INVALID_PASSWORD = "12345";
-}
